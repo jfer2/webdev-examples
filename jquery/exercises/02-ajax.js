8db1e9d7a46393db1eca178fb1565e8100c0ec59
jfer2 <jfer2@pdx.edu>
2021-11-15 12:20:42 -0800

    Cleaned up uncommented out code

--- jquery/exercises/02-ajax.js
+++ jquery/exercises/02-ajax.js
@@ -23,28 +23,7 @@ $(document).ready(() => {
       }
     });
 
-    // reference this code to write the jQuery code
-    // fetch(url)
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     data.forEach((item) => {
-    //       addBookToDOM(item);
-    //     });
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //     let li = document.createElement("li");
-    //     li.textContent = `An error occured. Please try again.`;
-    //     app.append(li);
-    //   })
-    //   .finally(() => {
-    //     app.removeChild(loading);
-    //   });
   };
-
-  // const app = document.querySelector("#books");
-  // app.style.paddingLeft = 0;
-  // const loading = document.querySelector("#loading");
 
   const addBookToDOM = (item) => {
     console.log(item);
@@ -62,16 +41,6 @@ $(document).ready(() => {
     .append($('<p>').text(`${item.numberOfPages} pages`))
     );
     
-    // element.style.display = "flex";
-    // element.style.flexDirection = "column";
-    // element.style.alignItems = "center";
-    // element.style.marginTop = "20px";
-
-    // title.textContent = item.name;
-    // author.textContent = `by ${item.authors[0]}`;
-    // published.textContent = item.released.substr(0, 4);
-    // pages.textContent = `${item.numberOfPages} pages`;
-    // app.append(element);
   };
 
   fetchData(url);
